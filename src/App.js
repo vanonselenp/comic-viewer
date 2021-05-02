728434522b51e3cfa969984dac0f5818fb4cfd18
Peter Van Onselen <peter.vanonselen@cazoo.co.uk>
2021-05-02 13:30:01 +0100

    initial react commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img class="fit-picture"
+          src="http://www.girlgeniusonline.com/ggmain/strips/ggmain20021104.jpg"
+          alt="Grapefruit slice atop a pile of other slices"/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
